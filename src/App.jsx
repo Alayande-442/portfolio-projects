4869b7a358ddec94ce0d705491254167ce8e6c25
Alayande-442 <alayandesteven@gmail.com>
2026-02-01 14:59:01 +0100

    feat: complete brandsLogo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./sections/Hero";
 import ProjectShowCase from "./sections/ProjectShowCase";
 import NavBar from "./components/NavBar";
+import BrandsSection from "./components/BrandsSection";
 const App = () => {
   return (
     <main>
@@ -8,6 +9,7 @@ const App = () => {
         <NavBar />
         <Hero />
         <ProjectShowCase />
+        <BrandsSection />
       </div>
     </main>
   );
